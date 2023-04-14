4b131854aa4e40c87fb96081810e91bb945bfbe5
Raj Heer <thisisrajmatazz@gmail.com>
2023-04-14 22:51:09 +0100

    Add h1 to header component and font, with webpack config.

--- src/header.js
+++ src/header.js
@@ -1,7 +1,11 @@
 export default function headerWithNav() {
     const header = document.createElement("header");
+    const h1 = document.createElement("h1");
     const nav = document.createElement("nav");
 
+    h1.innerText = "Mos Eisely Cantina"
+    
+    // NAV TABS
     const divAbout = document.createElement('div');
     divAbout.innerText = "ABOUT";
     const divMenu = document.createElement('div');
@@ -13,6 +17,7 @@ export default function headerWithNav() {
     nav.appendChild(divMenu);
     nav.appendChild(divContact);
 
+    header.appendChild(h1);
     header.appendChild(nav);
     
     return header;
